76f3d8bc62cb2e2c93642ee50db4f4ced3ec41b5
Pauline K <fishingtiger@mail.ru>
2025-03-14 20:30:41 +0200

    updated text-box search test

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,13 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize="1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
     @Test
     void fillFields() {
-        open("https://demoqa.com/text-box");
+        open("/text-box");
         $("#userName").setValue("User");
         $("#userEmail").setValue("User@mail.ru");
         $("#currentAddress").setValue("Калининград");
